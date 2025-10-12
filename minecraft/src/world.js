c07b05c05c53bda459a8b69933e58985c054e208
Guilherme Custodio Nieto <GuilhermeCustodioNieto@gmail.com>
2025-10-12 19:10:36 -0300

    feat: terrain generation

--- minecraft/src/world.js
+++ minecraft/src/world.js
@@ -1,15 +1,72 @@
 import * as THREE from "three";
+import { SimplexNoise } from "three/examples/jsm/math/SimplexNoise.js";
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({ color: 0x00d000 });
 
 export class World extends THREE.Group {
+  data = [];
+
+  params = {
+    terrain: {
+      scale: 30,
+      magnetude: 0.5,
+      offset: 0.2,
+    },
+  };
+
   constructor(size = { width: 64, height: 32 }) {
     super();
     this.size = size;
   }
 
   generate() {
+    this.initializeTerrain();
+    this.generateTerrain();
+    this.generateMeshes();
+  }
+
+  generateTerrain() {
+    const simplex = new SimplexNoise();
+    for (let x = 0; x < this.size.width; x++) {
+      for (let z = 0; z < this.size.width; z++) {
+        const value = simplex.noise(
+          x / this.params.terrain.scale,
+          z / this.params.terrain.scale
+        );
+
+        const scaleNoise =
+          this.params.terrain.offset + this.params.terrain.magnetude * value;
+
+        let height = Math.floor(this.size.height * scaleNoise);
+        height = Math.max(0, Math.min(height, this.size.height - 1));
+
+        for (let y = 0; y <= height; y++) {
+          this.setBlockId(x, y, z, 1);
+        }
+      }
+    }
+  }
+
+  initializeTerrain() {
+    this.data = [];
+    for (let x = 0; x < this.size.width; x++) {
+      const slice = [];
+      for (let y = 0; y < this.size.height; y++) {
+        const row = [];
+        for (let z = 0; z < this.size.width; z++) {
+          row.push({
+            id: 0,
+            instanceId: null,
+          });
+        }
+        slice.push(row);
+      }
+      this.data.push(slice);
+    }
+  }
+
+  generateMeshes() {
     this.clear();
     const maxCount = this.size.width * this.size.width * this.size.height;
     const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
@@ -20,11 +77,52 @@ export class World extends THREE.Group {
     for (let x = 0; x < this.size.width; x++) {
       for (let y = 0; y < this.size.height; y++) {
         for (let z = 0; z < this.size.width; z++) {
-          matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
-          mesh.setMatrixAt(mesh.count++, matrix);
+          const blockId = this.getBlock(x, y, z).id;
+          const instanceId = mesh.count;
+
+          if (blockId !== 0) {
+            matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+            mesh.setMatrixAt(instanceId, matrix);
+            this.setBlockInstanceId(x, y, z, instanceId);
+            mesh.count++;
+          }
         }
       }
     }
     this.add(mesh);
+  }
+
+  getBlock(x, y, z) {
+    if (this.inBounds(x, y, z)) {
+      return this.data[x][y][z];
+    } else {
+      return null;
+    }
+  }
+
+  setBlockId(x, y, z, id) {
+    if (this.inBounds(x, y, z)) {
+      this.data[x][y][z].id = id;
+    }
+  }
+
+  setBlockInstanceId(x, y, z, instanceId) {
+    if (this.inBounds(x, y, z)) {
+      this.data[x][y][z].instanceId = instanceId;
+    }
+  }
+
+  inBounds(x, y, z) {
+    if (
+      x >= 0 &&
+      x < this.size.width &&
+      y >= 0 &&
+      y < this.size.height &&
+      z >= 0 &&
+      z < this.size.width
+    ) {
+      return true;
+    }
+    return false;
   }
 }
